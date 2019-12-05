29f151cb97029c1fe2605e3b4a0b009929c9f8be
alexisvisco <alexis.viscogliosi@outlook.fr>
2019-12-05 11:25:35 +0100

    async: using asynchronous code instead of using threads

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ use std::env::args;
 use std::fs::File;
 use std::io::BufReader;
 use std::sync::mpsc;
+use crate::searcher::Occurrences;
+use std::future::Future;
+use futures::executor::block_on;
+use futures::future::join_all;
 
 mod searcher;
 mod reader;
 mod printer;
 
-const BUFFER_SIZE: usize = 4096;
+const BUFFER_SIZE: usize = 4096 * 10;
 
 /// This program is a grep that dispatch the searcher process into different threads.
 /// Then join the matched lines through a channel and finally print the lines.
@@ -24,36 +28,41 @@ fn main() {
     let file = File::open(filename.as_str());
 
     match file {
-        Ok(f) => search_in_file(&f, search),
+        Ok(f) => block_on(search_in_file(&f, search)),
         Err(err) => eprintln!("bgrep: {} : unable to read file {}", err, filename),
     }
+
 }
 
 
-fn search_in_file(file: &File, search: String) {
+async fn search_in_file(file: &File, search: String) {
     let mut buffer_reader = BufReader::with_capacity(BUFFER_SIZE, file);
     let mut chunk_index = 0;
 
-    let (tx, rx) = mpsc::channel::<searcher::Occurrences>();
+    let mut futures_vec = Vec::new();
 
     loop {
         let buffer = reader::read(&mut buffer_reader);
 
         match buffer {
             None => break,
-            Some(buffer) => searcher::search(tx.clone(), buffer, search.clone(), chunk_index),
+            Some(buffer) => futures_vec.push(searcher::search(buffer, search.clone(), chunk_index)),
         }
 
         chunk_index += 1;
     }
 
-    drop(tx);
 
     let mut occurrences: HashMap<usize, searcher::Occurrences> = HashMap::new();
-    for occurrence in rx {
-        occurrences.insert(occurrence.chunk_index, occurrence);
+    let result = join_all(futures_vec).await;
+
+    for may_occurrence in result {
+        if may_occurrence.is_some() {
+            let occurrence = may_occurrence.unwrap();
+            occurrences.insert(occurrence.chunk_index, occurrence);
+        }
     }
 
-    printer::print(&mut occurrences)
+    printer::print(&mut occurrences);
 }
 
